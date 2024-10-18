a656c9eac115881d902123d4e572d8b9c81d32d2
anjalichourasia <anjalichourasia1998@gmail.com>
2024-10-18 16:38:56 +0530

    Server listening

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-console.log("start");
+const express = require("express");
+const app = express(); //creating new express js application
+
+app.listen(3000, () => console.log("Server started on port 3000....")); // listening our web server on 3000 port
