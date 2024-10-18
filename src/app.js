f20d10639fecb923efd37f0544150f11acdc43ca
anjalichourasia <anjalichourasia1998@gmail.com>
2024-10-18 16:41:29 +0530

    Request Handler

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const app = express(); //creating new express js application
 
+//handing incoming request : Request Handler
+app.use((req, res) => {
+ res.send("Hello from server");
+})
+
 app.listen(3000, () => console.log("Server started on port 3000....")); // listening our web server on 3000 port
