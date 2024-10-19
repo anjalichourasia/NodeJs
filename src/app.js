0c801020617a6774e143f95e65ba0e8bc93042c7
anjalichourasia <anjalichourasia1998@gmail.com>
2024-10-19 14:27:44 +0530

    Connect db then add to server

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express(); //creating new express js application
-require("./config/database")
+const connectDB = require("./config/database")
 const { authentication } = require("./middleware/auth")
 const { err } = require("./middleware/err");
 
 //handing incoming request : Request Handler
 
 
-
-app.listen(3000, () => console.log("Server started on port 3000....")); // listening our web server on 3000 port\
+connectDB()
+    .then(() => {
+        console.log("Database is connected....");
+        app.listen(3000, () => console.log("Server started on port 3000....")); // listening our web server on 3000 port\
+    })
+    .catch(() => {
+        console.log("Database is throw error while connecting....");
+    })
 
 // put at the end of ur application
 app.use("/", err);
--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017"); // it return promise
+    await mongoose.connect("mongodb://127.0.0.1:27017/toDo"); // it return promise
 }
 
-connectDB()
-    .then(() => {
-        console.log("Database is connected....");
-    })
-    .catch(() => {
-        console.log("Database is throw error while connecting....");
-    })
+module.exports = connectDB
