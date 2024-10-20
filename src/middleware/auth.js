1d45d4ff286b3ec4d458db65137666bea781d068
anjalichourasia <anjalichourasia1998@gmail.com>
2024-10-20 11:36:28 +0530

    Added Middleware logic

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,27 @@
-const authentication = (req, res, next) => {
-    console.log("authenticated for admin");
-    const token = "xyz"
-    const isAuthenticated = token === "xyz";
-    if(!isAuthenticated){ 
-        res.status(401).send("UnAutherized")
-    } else {
+const jwt = require('jsonwebtoken');
+const UserDetail = require('../models/userDetail')
+
+const authentication = async (req, res, next) => {
+    try {
+        // Read the token from cookie
+        const cookie = req.cookie;
+
+        const { token } = cookie;
+        if(!token){
+            throw new Error("token is not valid");
+        }
+        
+        // process.env is best way
+        const decodedObj = await jwt.verify(token, "mySecret@123");
+        const { _id } = decodedObj;
+
+        const user = await UserDetail.findById(_id);
+        if(!user) {
+            throw new Error("User not found");
+        }
         next();
+    } catch (err) {
+        res.status(400).send(err.message);
     }
 };
 
